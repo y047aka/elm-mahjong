87a896448cded6b67cb145847dbe2e7c8bbf3970
Yoshitaka Totsuka <y047aka@gmail.com>
2024-04-27 12:14:00 +0900

    doctest

--- src/Data/Counter.elm
+++ src/Data/Counter.elm
@@ -7,6 +7,23 @@ type alias Counter =
     Array.Array Int
 
 
+{-|
+
+    import Array
+
+    fromIntList []
+    --> (Array.fromList [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ])
+
+    fromIntList [ 1, 9 ]
+    --> (Array.fromList [ 1, 0, 0, 0, 0, 0, 0, 0, 1 ])
+
+    fromIntList [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+    --> (Array.fromList [ 1, 1, 1, 1, 1, 1, 1, 1, 1 ])
+
+    fromIntList [ 1, 1, 1 ]
+    --> (Array.fromList [ 3, 0, 0, 0, 0, 0, 0, 0, 0 ])
+
+-}
 fromIntList : List Int -> Counter
 fromIntList intList =
     let

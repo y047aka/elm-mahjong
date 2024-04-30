853c3bc780ab5989499de5d0f9ecda3d1e3aeb62
Yoshitaka Totsuka <y047aka@gmail.com>
2024-04-30 12:35:02 +0900

    test: failed 10 cases

--- tests/ShantenTest.elm
+++ tests/ShantenTest.elm
@@ -51,33 +51,96 @@ suite =
             --     test10000_shantenChiitoitsu
             ]
         , describe "shantenStandard_debug"
-            [ test "0. 4556m33p2234457s1z" <|
+            [ test "3698. 1234579m446789p3s" <|
                 \() ->
                     shantenStandard_debug
-                        { man = [ [ Run M4 (M5 False) M6 ] ]
-                        , pin = [ [ Pair P3 P3 ] ]
-                        , sou = [ [ Run S2 S3 S4, PartialKanchan S2 S4, PartialKanchan (S5 False) S7 ], [ Pair S2 S2, Run S3 S4 (S5 False) ] ]
+                        { man = [ [ PartialPenchan M1 M2, Run M3 M4 (M5 False), PartialKanchan M7 M9 ], [ Run M1 M2 M3, PartialPenchan M4 (M5 False), PartialKanchan M7 M9 ] ]
+                        , pin = [ [ Pair P4 P4, PartialKanchan P6 P8, PartialKanchan P7 P9 ], [ Pair P4 P4, PartialPenchan P6 P7, PartialPenchan P8 P9 ], [ PartialKanchan P4 P6, Run P7 P8 P9 ] ]
+                        , sou = []
                         , honor = []
                         }
-                        |> Expect.equal 1
-            , test "5. 7778m23445p456s16z" <|
+                        |> Expect.equal 2
+            , test "4321. 3555778m2356p35s4z" <|
                 \() ->
                     shantenStandard_debug
-                        { man = [ [ Triplet M7 M7 M7 ] ]
-                        , pin = [ [ Run P2 P3 P4, PartialPenchan P4 (P5 False) ], [ PartialKanchan P2 P4, Run P3 P4 (P5 False) ] ]
-                        , sou = [ [ Run S4 (S5 False) S6 ] ]
+                        { man = [ [ PartialKanchan (M5 False) M7, Pair (M5 False) (M5 False), PartialPenchan M7 M8 ], [ Pair (M5 False) (M5 False), PartialKanchan (M5 False) M7, PartialPenchan M7 M8 ], [ PartialKanchan M3 (M5 False), Pair (M5 False) (M5 False), PartialPenchan M7 M8 ], [ Triplet (M5 False) (M5 False) (M5 False), PartialPenchan M7 M8 ] ]
+                        , pin = [ [ PartialPenchan P2 P3, PartialPenchan (P5 False) P6 ] ]
+                        , sou = [ [ PartialKanchan S3 (S5 False) ] ]
                         , honor = []
                         }
-                        |> Expect.equal 1
-            , test "28. 147m234666p56678s" <|
+                        |> Expect.equal 3
+            , test "5762. 2389m16p13445679s" <|
                 \() ->
                     shantenStandard_debug
-                        { man = []
-                        , pin = [ [ Run P2 P3 P4, Triplet P6 P6 P6 ] ]
-                        , sou = [ [ Run (S5 False) S6 S7, PartialKanchan S6 S8 ], [ PartialPenchan (S5 False) S6, Run S6 S7 S8 ] ]
+                        { man = [ [ PartialPenchan M2 M3, PartialPenchan M8 M9 ] ]
+                        , pin = []
+                        , sou = [ [ PartialKanchan S1 S3, PartialKanchan S4 S6, PartialPenchan S4 (S5 False), PartialKanchan S7 S9 ], [ PartialKanchan S1 S3, PartialPenchan S4 (S5 False), PartialKanchan S4 S6, PartialKanchan S7 S9 ], [ PartialPenchan S3 S4, Run S4 (S5 False) S6, PartialKanchan S7 S9 ], [ Run S3 S4 (S5 False), PartialKanchan S4 S6, PartialKanchan S7 S9 ], [ PartialKanchan S1 S3, Run S4 (S5 False) S6, PartialKanchan S7 S9 ], [ PartialKanchan S1 S3, Pair S4 S4, PartialPenchan (S5 False) S6, PartialKanchan S7 S9 ] ]
                         , honor = []
                         }
-                        |> Expect.equal 1
+                        |> Expect.equal 3
+            , test "6207. 1679m89p12557789s" <|
+                \() ->
+                    shantenStandard_debug
+                        { man = [ [ PartialKanchan M7 M9 ], [ PartialPenchan M6 M7 ] ]
+                        , pin = [ [ PartialPenchan P8 P9 ] ]
+                        , sou = [ [ PartialPenchan S1 S2, PartialKanchan (S5 False) S7, PartialKanchan (S5 False) S7, PartialPenchan S8 S9 ], [ PartialPenchan S1 S2, PartialKanchan (S5 False) S7, Run S7 S8 S9 ], [ PartialPenchan S1 S2, Pair (S5 False) (S5 False), PartialKanchan S7 S9, PartialPenchan S7 S8 ], [ PartialPenchan S1 S2, Pair (S5 False) (S5 False), PartialPenchan S7 S8, PartialKanchan S7 S9 ] ]
+                        , honor = []
+                        }
+                        |> Expect.equal 3
+            , test "6212. 234556m13579p167s" <|
+                \() ->
+                    shantenStandard_debug
+                        { man = [ [ PartialKanchan M2 M4, PartialKanchan M3 (M5 False), PartialPenchan (M5 False) M6 ], [ PartialPenchan M2 M3, PartialPenchan M4 (M5 False), PartialPenchan (M5 False) M6 ], [ PartialKanchan M3 (M5 False), Run M4 (M5 False) M6 ], [ Run M3 M4 (M5 False), PartialPenchan (M5 False) M6 ], [ PartialPenchan M2 M3, Run M4 (M5 False) M6 ], [ Run M2 M3 M4, PartialPenchan (M5 False) M6 ], [ PartialPenchan M2 M3, PartialKanchan M4 M6, Pair (M5 False) (M5 False) ] ]
+                        , pin = [ [ PartialKanchan P3 (P5 False), PartialKanchan P7 P9 ], [ PartialKanchan P1 P3, PartialKanchan P7 P9 ], [ PartialKanchan P1 P3, PartialKanchan (P5 False) P7 ] ]
+                        , sou = [ [ PartialPenchan S6 S7 ] ]
+                        , honor = []
+                        }
+                        |> Expect.equal 3
+            , test "6380. 458m567889p2357s7z" <|
+                \() ->
+                    shantenStandard_debug
+                        { man = [ [ PartialPenchan M4 (M5 False) ] ]
+                        , pin = [ [ PartialKanchan (P5 False) P7, PartialKanchan P6 P8, PartialPenchan P8 P9 ], [ PartialPenchan (P5 False) P6, PartialPenchan P7 P8, PartialPenchan P8 P9 ], [ PartialKanchan P6 P8, Run P7 P8 P9 ], [ Run P6 P7 P8, PartialPenchan P8 P9 ], [ PartialPenchan (P5 False) P6, Run P7 P8 P9 ], [ Run (P5 False) P6 P7, PartialPenchan P8 P9 ], [ PartialPenchan (P5 False) P6, PartialKanchan P7 P9, Pair P8 P8 ] ]
+                        , sou = [ [ PartialPenchan S2 S3, PartialKanchan (S5 False) S7 ] ]
+                        , honor = []
+                        }
+                        |> Expect.equal 3
+            , test "6896. 6779m12556778s46z" <|
+                \() ->
+                    shantenStandard_debug
+                        { man = [ [ PartialPenchan M6 M7, PartialKanchan M7 M9 ], [ Pair M7 M7 ] ]
+                        , pin = []
+                        , sou = [ [ PartialPenchan S1 S2, PartialKanchan (S5 False) S7, PartialKanchan (S5 False) S7, PartialKanchan S6 S8 ], [ PartialPenchan S1 S2, PartialKanchan (S5 False) S7, PartialPenchan (S5 False) S6, PartialPenchan S7 S8 ], [ PartialPenchan S1 S2, PartialPenchan (S5 False) S6, PartialKanchan (S5 False) S7, PartialPenchan S7 S8 ], [ PartialPenchan S1 S2, PartialKanchan (S5 False) S7, Run S6 S7 S8 ], [ PartialPenchan S1 S2, PartialKanchan (S5 False) S7, Run (S5 False) S6 S7 ], [ PartialPenchan S1 S2, Run (S5 False) S6 S7, PartialPenchan S7 S8 ], [ PartialPenchan S1 S2, Run (S5 False) S6 S7, PartialKanchan (S5 False) S7 ], [ PartialPenchan S1 S2, Pair (S5 False) (S5 False), PartialKanchan S6 S8, Pair S7 S7 ] ]
+                        , honor = []
+                        }
+                        |> Expect.equal 3
+            , test "8190. 479m234556p6889s7z" <|
+                \() ->
+                    shantenStandard_debug
+                        { man = [ [ PartialKanchan M7 M9 ] ]
+                        , pin = [ [ PartialKanchan P2 P4, PartialKanchan P3 (P5 False), PartialPenchan (P5 False) P6 ], [ PartialPenchan P2 P3, PartialPenchan P4 (P5 False), PartialPenchan (P5 False) P6 ], [ PartialKanchan P3 (P5 False), Run P4 (P5 False) P6 ], [ Run P3 P4 (P5 False), PartialPenchan (P5 False) P6 ], [ PartialPenchan P2 P3, Run P4 (P5 False) P6 ], [ Run P2 P3 P4, PartialPenchan (P5 False) P6 ], [ PartialPenchan P2 P3, PartialKanchan P4 P6, Pair (P5 False) (P5 False) ] ]
+                        , sou = [ [ PartialKanchan S6 S8, PartialPenchan S8 S9 ], [ Pair S8 S8 ] ]
+                        , honor = []
+                        }
+                        |> Expect.equal 3
+            , test "9194. 24m567p122467889s" <|
+                \() ->
+                    shantenStandard_debug
+                        { man = [ [ PartialKanchan M2 M4 ] ]
+                        , pin = [ [ Run (P5 False) P6 P7 ], [ PartialPenchan P6 P7 ], [ PartialKanchan (P5 False) P7 ], [ PartialPenchan (P5 False) P6 ] ]
+                        , sou = [ [ PartialPenchan S1 S2, PartialKanchan S2 S4, PartialKanchan S6 S8, Run S7 S8 S9 ], [ PartialPenchan S1 S2, PartialKanchan S2 S4, Run S6 S7 S8, PartialPenchan S8 S9 ], [ Pair S2 S2, PartialKanchan S4 S6, PartialKanchan S7 S9, Pair S8 S8 ] ]
+                        , honor = []
+                        }
+                        |> Expect.equal 2
+            , test "9593. 25668m224456p157s" <|
+                \() ->
+                    shantenStandard_debug
+                        { man = [ [ PartialPenchan (M5 False) M6, PartialKanchan M6 M8 ], [ Pair M6 M6 ] ]
+                        , pin = [ [ PartialKanchan P2 P4, PartialKanchan P2 P4, PartialPenchan (P5 False) P6 ], [ PartialKanchan P2 P4, Run P4 (P5 False) P6 ], [ Pair P2 P2, PartialKanchan P4 P6, PartialPenchan P4 (P5 False) ], [ Pair P2 P2, PartialPenchan P4 (P5 False), PartialKanchan P4 P6 ] ]
+                        , sou = [ [ PartialKanchan (S5 False) S7 ] ]
+                        , honor = []
+                        }
+                        |> Expect.equal 3
             ]
         ]
 
